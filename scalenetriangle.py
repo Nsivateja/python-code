e0860ec5db9d324872cc41d53647010735d88a90
Nsivateja <112919241+Nsivateja@users.noreply.github.com>
2022-09-06 13:04:27 +0530

    Add files via upload

---
+++ scalenetriangle.py
@@ -0,0 +1,10 @@
+Python 3.7.0 (v3.7.0:1bf9cc5093, Jun 27 2018, 04:06:47) [MSC v.1914 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> 
+ RESTART: C:/Users/sse/AppData/Local/Programs/Python/Python37-32/pascalstriangle.py 
+Input lengths of the triangle sides: 
+x: 5
+y: 4
+z: 8
+Scalene triangle
+>>> 
