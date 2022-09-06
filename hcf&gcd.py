a5fa85cd18e9acae7e3c62b32ca61e5a2ed4beb2
Nsivateja <112919241+Nsivateja@users.noreply.github.com>
2022-09-06 12:54:15 +0530

    Add files via upload

---
+++ hcf&gcd.py
@@ -0,0 +1,8 @@
+Python 3.7.0 (v3.7.0:1bf9cc5093, Jun 27 2018, 04:06:47) [MSC v.1914 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> 
+ RESTART: C:/Users/sse/AppData/Local/Programs/Python/Python37-32/pascalstriangle.py 
+Enter first number: 16
+Enter second number: 20
+The H.C.F. of 16 and 20 is 4 
+>>> 
