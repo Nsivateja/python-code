fd056fcc5c400b4b865590bb5cdc4b6ac240db46
Nsivateja <112919241+Nsivateja@users.noreply.github.com>
2022-09-06 13:11:31 +0530

    Add files via upload

---
+++ no of digits & alphabets.py
@@ -0,0 +1,8 @@
+Python 3.7.0 (v3.7.0:1bf9cc5093, Jun 27 2018, 04:06:47) [MSC v.1914 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> 
+ RESTART: C:/Users/sse/AppData/Local/Programs/Python/Python37-32/pascalstriangle.py 
+Please Enter Your Own String : shiva95
+Total Number of Vowels in this String =  2
+Total Number of Consonants in this String =  5
+>>> 
