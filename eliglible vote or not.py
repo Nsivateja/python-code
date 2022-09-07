ce502061723d71cb336b9482777102832eb08ccb
Nsivateja <112919241+Nsivateja@users.noreply.github.com>
2022-09-07 10:37:06 +0530

    Add files via upload

---
+++ eliglible vote or not.py
@@ -0,0 +1,15 @@
+Python 3.7.0 (v3.7.0:1bf9cc5093, Jun 27 2018, 04:06:47) [MSC v.1914 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> 
+========== RESTART: C:/Users/sse/Desktop/user name valid or not.py ==========
+enter the number15
+you are not eligible to vote  3
+>>> 
+========== RESTART: C:/Users/sse/Desktop/user name valid or not.py ==========
+enter the number18
+you are eligible to vote
+>>> 
+========== RESTART: C:/Users/sse/Desktop/user name valid or not.py ==========
+enter the number34
+you are eligible to vote
+>>> 
